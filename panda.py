8d6f7c947a00ab28f596df394a98d152ef349834
Luciano Resende <lresende@apache.org>
2019-10-15 15:10:50 -0700

    Remove sample driver code

--- panda.py
+++ panda.py
@@ -7,6 +7,3 @@ def df_from_url(url):
     df = pd.read_csv(io.StringIO(data.decode('utf-8')))
     return df
 
-
-df = df_from_url('http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv')
-df.groupby('zip')['price'].mean()
