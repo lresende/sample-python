5252d8756aa224248ef584ca5673135589b80946
Luciano Resende <lresende@apache.org>
2020-01-30 15:38:38 -0800

    Add sample panda code to manipulate the generted df

--- panda.py
+++ panda.py
@@ -7,3 +7,13 @@ def df_from_url(url):
     df = pd.read_csv(io.StringIO(data.decode('utf-8')))
     return df
 
+# Uncomment the lines below to sleep for a bit
+# useful to demonstrate kernel startup on container environments
+import time
+time.sleep(5)
+
+# Sample panda code to manipulate the generated data frame 
+# and calculate mean price per zipcode
+df = df_from_url('http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv')
+df.groupby('zip')['price'].mean()    
+
