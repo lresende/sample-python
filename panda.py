8a839b8ad65c7124eb11f194562ca14232adab9e
Luciano Resende <lresende@apache.org>
2020-11-13 07:32:50 -0800

    Fix lint issues

--- panda.py
+++ panda.py
@@ -1,14 +1,18 @@
 import io
-import requests
 import pandas as pd
+import requests
+import time
+
 
 # Format output
 pd.options.display.float_format = '${:,.2f}'.format
 
+
 def delay(seconds):
     time.sleep(seconds)
 
-def df_from_url(url):
+
+def df_from_url(url: str):
     data = requests.get(url).content
     df = pd.read_csv(io.StringIO(data.decode('utf-8')))
     return df
@@ -17,10 +21,8 @@ def df_from_url(url):
 # useful to demonstrate kernel startup on container environments
 # delay(5)
 
+
 # Sample panda code to manipulate the generated data frame 
 # and calculate mean price per city/zipcode
 df = df_from_url('https://raw.githubusercontent.com/lresende/sample-python/master/data/Sacramento-RealEstate-Transactions.csv')
-df.groupby('city')['price'].mean()    
-
-
-
+df.groupby('city')['price'].mean()
